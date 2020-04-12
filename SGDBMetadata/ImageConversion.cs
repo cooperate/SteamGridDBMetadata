9b42a8c8374d7e539434a088f78f48c0f75e4652
cooperate1 <brett.cizmar@hotmail.com>
2020-04-12 16:45:57 -0700

    Add initial implementation for Image Conversion.

---
+++ SGDBMetadata/ImageConversion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SGDBMetadata
+{
+    class ImageConversion
+    {
+        public byte[] AsJpeg(byte[] data)
+        {
+            using (var inStream = new MemoryStream(data))
+            using (var outStream = new MemoryStream())
+            {
+                var imageStream = Image.FromStream(inStream);
+                imageStream.Save(outStream, ImageFormat.Jpeg);
+                return outStream.ToArray();
+            }
+        }
+
+        public byte[] Resize(byte[] data, int width)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                var image = Image.FromStream(stream);
+
+                var height = (width * image.Height) / image.Width;
+                var thumbnail = image.GetThumbnailImage(width, height, null, IntPtr.Zero);
+
+                using (var thumbnailStream = new MemoryStream())
+                {
+                    thumbnail.Save(thumbnailStream, ImageFormat.Jpeg);
+                    return thumbnailStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ SGDBMetadata/ImageDownloader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SGDBMetadata
+{
+    class ImageDownloader
+    {
+        private bool _disposed;
+        private readonly HttpClient _httpClient;
+
+        public ImageDownloader()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// Downloads an image asynchronously from the <paramref name="uri"/> and places it in the specified <paramref name="directoryPath"/> with the specified <paramref name="fileName"/>.
+        /// </summary>
+        /// <param name="directoryPath">The relative or absolute path to the directory to place the image in.</param>
+        /// <param name="fileName">The name of the file without the file extension.</param>
+        /// <param name="uri">The URI for the image to download.</param>
+        public async Task DownloadImage(string directoryPath, string fileName, Uri uri)
+        {
+            if (_disposed) { throw new ObjectDisposedException(GetType().FullName); }
+
+            // Get the file extension
+            var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+            var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+            // Create file path and ensure directory exists
+            var path = Path.Combine(directoryPath, $"{fileName}{fileExtension}");
+            Directory.CreateDirectory(directoryPath);
+
+            // Download the image and write to the file
+            var imageBytes = await _httpClient.GetByteArrayAsync(uri);
+            File.WriteAllBytes(path, imageBytes);
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) { return; }
+            _httpClient.Dispose();
+            GC.SuppressFinalize(this);
+            _disposed = true;
+        }
+    }
+}
