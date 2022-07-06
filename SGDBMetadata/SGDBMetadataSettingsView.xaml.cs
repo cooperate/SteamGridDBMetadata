4ed6f1727f4fd705aaa311e46c7bca6fc79b5e55
Brandon <brandon_corchado1@hotmail.com>
2022-07-05 20:17:12 -0500

    Added items to support querying  covers of 460x215 & 920x430 and 512x512 & 1024x1024 at the same time

--- SGDBMetadata/SGDBMetadataSettingsView.xaml.cs
+++ SGDBMetadata/SGDBMetadataSettingsView.xaml.cs
@@ -54,11 +54,13 @@ namespace SGDBMetadata
                 { "any", ResourceProvider.GetString("LOCSteamGridDBMetadata_SettingGenericAny") },
                 { "460x215", "460x215" },
                 { "920x430",  "920x430" },
+                { "460x215,920x430", "460x215 & 920x430" },
                 { "600x900", "600x900" },
                 { "342x482", "342x482" },
                 { "660x930", "660x930" },
                 { "512x512", "512x512" },
-                { "1024x1024", "1024x1024" }
+                { "1024x1024", "1024x1024" },
+                { "512x512,1024x1024", "512x512 & 1024x1024" }
             };
 
             cmbCoverNsfw.ItemsSource = cmbGenericNsfw;
